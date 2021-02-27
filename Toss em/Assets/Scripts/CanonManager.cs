163f68efff3f8c834971f7fada57ee246a190036
anulagarwal <anulagarwal12@gmail.com>
2021-02-27 11:50:07 +0530

    Project Setup

---
+++ Toss em/Assets/Scripts/CanonManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonManager : MonoBehaviour
+{
+    public GameObject human;
+    [SerializeField] private float cooldown;
+    [SerializeField] private float maxHuman;
+
+
+    #region MonoBehaviour functions
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+
+    public void Shoot()
+    {
+        //Add Force to human in the direction pointing
+    }
+}
---
+++ Toss em/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    #region MonoBehaviour function
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    public void WinLevel()
+    {
+
+    }
+
+    public void LoseLevel()
+    {
+
+    }
+
+    public void LoadLevel()
+    {
+
+    }
+
+    public void StartLevel()
+    {
+
+    }
+}
